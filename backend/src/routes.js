23dd54ff489e2dad4e0eb5e3f3863794665b0da5
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-07-17 18:48:56 -0300

    Criando estrutura

---
+++ backend/src/routes.js
@@ -0,0 +1,9 @@
+//O router permite separar as rotas em outro file
+const { Router } = require('express');
+
+const routes = new Router();
+
+routes.get('/', (request, response) => {
+    return response.json({message: 'Hello World'})
+});
+module.exports = routes;    
---
+++ backend/src/server.js
@@ -0,0 +1,4 @@
+//Parte em que eu crio o servidor
+const app = require('./app');
+
+app.listen(3333);
