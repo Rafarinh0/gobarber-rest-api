d2c86d88c031ab94284e89a30adb94a94ebca4eb
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-08-02 16:04:25 -0300

    Cancelamento de agendamentos

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -3,7 +3,7 @@ import User from '../models/User';
 import File from '../models/File';
 import * as Yup from 'yup';
 import pt from 'date-fns/locale/pt';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import Notification from '../schemas/Notification';
 
 class AppointmentController {
@@ -95,6 +95,29 @@ class AppointmentController {
             content: `Novo agendamento de ${user.name} para ${formattedDate}`,
             user: provider_id,
         });
+
+        return response.json(appointment);
+    }
+
+    async delete(request, response) {
+        const appointment = await Appointment.findByPk(request.params.id);
+
+        if (appointment.user_id !== request.userId) {
+            return response.status(401).json({ error: "You don't have permission to cancel this appointment" })
+
+        }
+
+        const dateWithSub = subHours(appointment.date, 2);
+
+        if (isBefore(dateWithSub, new Date())) {
+            return response.status(401).json({
+                error: 'You can only cancel appointments 2 hours in advance'
+            })
+        }
+
+        appointment.canceled_at = new Date();
+
+        await appointment.save();
 
         return response.json(appointment);
     }
