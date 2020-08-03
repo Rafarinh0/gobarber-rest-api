cd75c3f36275f5bc5aa2e4dc109c66b2a16af732
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-08-03 19:45:00 -0300

    Tratamento de exceções e monitoramento de erros com o Sentry.io

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
 // Estrutura da aplicação
 import express from "express";
-import routes from "./routes";
 import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+import routes from "./routes";
+import SentryConfig from './config/sentry';
+
 
 import './database';
 
@@ -9,11 +14,14 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(SentryConfig);
+
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +30,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (error, request, response, next) => {
+      const errors = await new Youch(error, request).toJSON();
+      return response.status(500).json(errors);
+    })
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://cba76f123f1649798aacada1a717b2c7@o429461.ingest.sentry.io/5376101'
+}
