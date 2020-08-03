4321b3bcd7c4f1881ba0899a281fa4af6f270c16
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-08-03 16:18:20 -0300

    Configurando fila com o Redis

---
+++ backend/src/lib/Queue.js
@@ -0,0 +1,41 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+    constructor() {
+        this.queues = {};
+        this.init();
+    }
+
+    init() {
+        //Pra cada um desses jobs eu crio uma fila
+        //Dentro dessa fila eu armazeno o bee, que conecta com o redis e armazena e recupera valores do banco de daados
+        //e armazeno o handle que processa a fila
+        jobs.forEach(({ key, handle }) => {
+            this.queues[key] = {
+                bee: new Bee(key, {
+                    redis: redisConfig
+                }),
+                handle,
+            };
+        });
+    }
+
+    add(queue, job) {
+        return this.queues[queue].bee.createJob(job).save();
+    }
+
+    //Pega cada um desses jobs e processa em tempo real
+    processQueue() {
+        jobs.forEach(job => {
+            const { bee, handle } = this.queues[job.key];
+
+            bee.process(handle);
+        })
+    }
+}
+
+export default new Queue();
